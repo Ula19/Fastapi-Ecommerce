4fb63507cab85b593287100f87ed27e8197702e4
Ula19 <ulug.2000.2@gmail.com>
2026-02-17 09:42:38 +0500

    Добавлена связь между ForeignKey между таблицами Category и Product

--- app/models/category.py
+++ app/models/category.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
-from sqlalchemy.orm import relationship
-
 from app.backend.db import Base
-
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models import *
 
 class Category(Base):
-    __tablename__ = 'category'
+    __tablename__ = 'categories'
 
-    id = Column(Integer, primary_key=True, unique=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     slug = Column(String, unique=True, index=True)
     is_active = Column(Boolean, default=True)
+    parent_id = Column(Integer, ForeignKey('categories.id'), nullable=True)
+
+    products = relationship("Product", back_populates="category")
