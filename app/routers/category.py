6aaf7b75c0eb4ebc0a59277f28e49e50013d1d19
Ula19 <ulug.2000.2@gmail.com>
2026-02-19 13:00:05 +0500

    CRUD операции категорий и продуктов сделали асинхронными

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,6 +1,6 @@
 from slugify import slugify
 from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import insert, select, update, delete
 from typing import Annotated
 
@@ -13,25 +13,25 @@ router = APIRouter(prefix='/category', tags=['category'])
 
 
 @router.get('/all_categories')
-async def get_all_categories(db: Annotated[Session, Depends(get_db)]):
+async def get_all_categories(db: Annotated[AsyncSession, Depends(get_db)]):
     """
     Метод получения всех категорий
     """
 
-    categories = db.scalars(select(Category).where(Category.is_active == True)).all()
-    return categories
+    categories = await db.scalars(select(Category).where(Category.is_active == True))
+    return categories.all()
 
 
 @router.post('/create', status_code=status.HTTP_201_CREATED)
-async def create_category(db: Annotated[Session, Depends(get_db)], create_category: CreateCategory):
+async def create_category(db: Annotated[AsyncSession, Depends(get_db)], create_category: CreateCategory):
     """
     Метод создания категории
     """
     try:
-        db.execute(insert(Category).values(name=create_category.name,
+         await db.execute(insert(Category).values(name=create_category.name,
                                            parent_id=create_category.parent_id,
                                            slug=slugify(create_category.name)))
-        db.commit()
+         await db.commit()
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Такая категрия уже существует')
     return {
@@ -42,21 +42,21 @@ async def create_category(db: Annotated[Session, Depends(get_db)], create_catego
 
 
 @router.put('/update_category')
-async def update_category(db: Annotated[Session, Depends(get_db)], category_id: int, update_category: CreateCategory):
+async def update_category(db: Annotated[AsyncSession, Depends(get_db)], category_id: int, update_category: CreateCategory):
     """
     Метод изменения категории
     """
 
-    category = db.scalar(select(Category).where(Category.id == category_id))
+    category = await db.scalar(select(Category).where(Category.id == category_id))
     if category is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Категория не найдена")
-    db.execute(update(Category).where(Category.id == category_id).values(
+    await db.execute(update(Category).where(Category.id == category_id).values(
         name=update_category.name,
         slug=slugify(update_category.name),
         parent_id=update_category.parent_id
     ))
-    db.commit()
+    await db.commit()
     return {
         "status_code": status.HTTP_200_OK,
         "transaction": "Обновление категории прошло успешно"
@@ -65,17 +65,17 @@ async def update_category(db: Annotated[Session, Depends(get_db)], category_id: 
 
 
 @router.delete('/delete')
-async def delete_category(db: Annotated[Session, Depends(get_db)], category_id: int):
+async def delete_category(db: Annotated[AsyncSession, Depends(get_db)], category_id: int):
     """
     Метод удаления категории
     """
 
-    category = db.scalar(select(Category).where(Category.id == category_id))
+    category = await db.scalar(select(Category).where(Category.id == category_id))
     if category is None:
        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                            detail="Категория не найдена")
-    db.execute(update(Category).where(Category.id == category_id).values(is_active=False))  # Мягкое удаление
-    # db.execute(delete(Category).where(Category.id == category_id))  # Жесткое удаление
-    db.commit()
+    await db.execute(update(Category).where(Category.id == category_id).values(is_active=False))  # Мягкое удаление
+    # await db.execute(delete(Category).where(Category.id == category_id))  # Жесткое удаление
+    await db.commit()
     return {"status_code": status.HTTP_200_OK,
             "transaction": "Удаление категории прошло успешно"}
--- app/routers/products.py
+++ app/routers/products.py
@@ -1,6 +1,6 @@
 from slugify import slugify
 from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, insert, update, delete
 from typing import Annotated
 
@@ -13,24 +13,24 @@ router = APIRouter(prefix='/products', tags=['products'])
 
 
 @router.get('/')
-async def all_products(db: Annotated[Session, Depends(get_db)]):
+async def all_products(db: Annotated[AsyncSession, Depends(get_db)]):
     """
     Метод получения всех товаров
     """
 
-    products = db.scalars(select(Product).where(Product.is_active == True, Product.stock > 0)).all()
+    products = await db.scalars(select(Product).where(Product.is_active == True, Product.stock > 0))
     if not products:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Нет товаров")
-    return products
+    return products.all()
 
 
 @router.post('/create')
-async def create_product(db: Annotated[Session, Depends(get_db)], create_product: CreateProduct):
+async def create_product(db: Annotated[AsyncSession, Depends(get_db)], create_product: CreateProduct):
     """
     Метод создания товара
     """
 
-    db.execute(insert(Product).values(name=create_product.name,
+    await db.execute(insert(Product).values(name=create_product.name,
                                       description=create_product.description,
                                       price=create_product.price,
                                       image_url=create_product.image_url,
@@ -38,7 +38,7 @@ async def create_product(db: Annotated[Session, Depends(get_db)], create_product
                                       slug=slugify(create_product.name),
                                       rating='0.0',
                                       category_id=create_product.category))
-    db.commit()
+    await db.commit()
     return {
         "status_code": status.HTTP_201_CREATED,
         "transaction": "Успешный"
@@ -46,31 +46,31 @@ async def create_product(db: Annotated[Session, Depends(get_db)], create_product
 
 
 @router.get('/{category_slug}')
-async def product_by_category(category_slug: str, db: Annotated[Session, Depends(get_db)]):
+async def product_by_category(db: Annotated[AsyncSession, Depends(get_db)], category_slug: str):
     """
     Метод получения товаров определенной категории
     """
 
-    category = db.scalar(select(Category).where(Category.slug == category_slug))
+    category = await db.scalar(select(Category).where(Category.slug == category_slug))
     if category is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Категория не найдена")
-    subcategories = db.scalars(select(Category).where(Category.parent_id == category.id)).all()
+    subcategories = await db.scalars(select(Category).where(Category.parent_id == category.id)).all()
     categories_and_subcategories = [category.id] + [i.id for i in subcategories]
-    products_category = db.scalars(
+    products_category = await db.scalars(
         select(Product).where(Product.category_id.in_(categories_and_subcategories),
                               Product.is_active == True, Product.stock > 0)
-    ).all()
-    return products_category
+    )
+    return products_category.all()
 
 
 @router.get('/detail/{product_slug}')
-async def product_detail(product_slug: str, db: Annotated[Session, Depends(get_db)]):
+async def product_detail(db: Annotated[AsyncSession, Depends(get_db)], product_slug: str):
     """
     Метод получения детальной информации о товаре
     """
 
-    product = db.scalar(
+    product = await db.scalar(
         select(Product).where(Product.slug == product_slug, Product.is_active == True, Product.stock > 0)
     )
     if product is None:
@@ -79,16 +79,16 @@ async def product_detail(product_slug: str, db: Annotated[Session, Depends(get_d
 
 
 @router.put('/detail/{product_slug}')
-async def update_product(product_slug: str, db: Annotated[Session, Depends(get_db)], update_product_model: CreateProduct):
+async def update_product(db: Annotated[AsyncSession, Depends(get_db), product_slug: str], update_product_model: CreateProduct):
     """
     Метод изменения товара
     """
 
-    product = db.scalar(select(Product).where(Product.slug == product_slug))
+    product = await db.scalar(select(Product).where(Product.slug == product_slug))
     if product is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Товар не найден")
-    db.execute(update(Product).where(Product.slug == product_slug).values(
+    await db.execute(update(Product).where(Product.slug == product_slug).values(
         name=update_product_model.name,
         description=update_product_model.description,
         price=update_product_model.price,
@@ -97,7 +97,7 @@ async def update_product(product_slug: str, db: Annotated[Session, Depends(get_d
         category_id=update_product_model.category,
         slug=slugify(update_product_model.name),
     ))
-    db.commit()
+    await db.commit()
     return {
         "status_code": status.HTTP_200_OK,
         "transaction": "Обновление категории прошло успешно"
@@ -105,18 +105,18 @@ async def update_product(product_slug: str, db: Annotated[Session, Depends(get_d
 
 
 @router.delete('/delete/{product_slug}')
-async def delete_product(product_slug: str, db: Annotated[Session, Depends(get_db)]):
+async def delete_product(db: Annotated[AsyncSession, Depends(get_db)], product_slug: str):
     """
     Метод удаления товара
     """
 
-    product = db.scalar(select(Product).where(Product.slug == product_slug))
+    product = await db.scalar(select(Product).where(Product.slug == product_slug))
     if product is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Товар не найден")
-    db.execute(update(Product).where(Product.slug == product_slug).values(is_active=False))  # Мягкое удаление
-    # db.execute(delete(Product).where(Product.slug == product_slug))  # Жесткое удаление
-    db.commit()
+    await db.execute(update(Product).where(Product.slug == product_slug).values(is_active=False))  # Мягкое удаление
+    # await db.execute(delete(Product).where(Product.slug == product_slug))  # Жесткое удаление
+    await db.commit()
     return {
         "status_code": status.HTTP_200_OK,
         "transaction": "Удаление продукта прошло успешно"
