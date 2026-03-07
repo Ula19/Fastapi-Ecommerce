ceaa841e88c15e509f92d7802267449056e28f7a
Ula19 <ulug.2000.2@gmail.com>
2026-03-07 13:17:23 +0500

    add Docker

--- app/backend/db.py
+++ app/backend/db.py
@@ -2,7 +2,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
-engine = create_async_engine('postgresql+asyncpg://ecommerce:patronus@localhost:5432/ecommerce', echo=True)
+# engine = create_async_engine('postgresql+asyncpg://ecommerce:patronus@localhost:5432/ecommerce', echo=True)
+engine = create_async_engine('postgresql+asyncpg://postgres_user:postgres_password@db:5432/postgres_database',
+                             echo=False)
 
 # типичная форма URL-адреса базы данных для PostgreSQL
 # engine = create_engine('postgresql+asyncpg://ecommerce:xxxxxx@localhost:5432/ecommerce')
